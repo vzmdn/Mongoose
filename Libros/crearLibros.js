196d5465971d87e3eb6225fbd1a930c64a1d1161
Carlos Vozmediano <carlosvozmedianoruiz@gmail.com>
2025-01-31 22:41:39 +0100

    Add initial Libros project structure with Electron setup and book management functionality

--- Libros/crearLibros.js
+++ Libros/crearLibros.js
@@ -1,14 +1,66 @@
-const container = document.getElementById('container');
+//const mongoose = require('mongoose');
+//connect('mongodb://localhost:27017/libros');
+let bookArray = [];
 
+cargarLibrosJSON();
 
-fetch('files/books.json').then(response => {
-    return response.json()
-    .then(books => {
-        books.forEach(book => {
-            const div = document.createElement('div');
-            div.id = book.img.replace(/^\D+/g, '');
-            div.innerHTML = book.title;
-            container.appendChild(div);
+//guardarLibrosMongo();
+
+function cargarLibrosJSON() {
+    bookArray = [];
+    const container = document.getElementById('container');
+    fetch('files/books.json').then(response => {
+        return response.json()
+            .then(books => {
+                books.forEach(book => {
+                    bookArray.push(book);
+                    const div = document.createElement('div');
+                    div.id = book.img.replace(/^\D+/g, '');
+                    const img = document.createElement('img');
+                    img.src = `files/${book.img}`;
+                    img.title = `${book.title}\n${book.author}`;
+                    div.appendChild(img);
+                    container.appendChild(div);
+                })
+            })
+    });
+}
+
+function guardarLibrosMongo() {
+    connect('mongodb://localhost:27017/libros');
+    let libroSchema = new Schema({
+        title: {
+            type: String,
+            required: true,
+            minlength: 1,
+            trim: true
+        },
+        author: {
+            type: String,
+            required: true,
+            minlength: 1,
+            trim: true
+        },
+        img: {
+            type: String,
+            required: true,
+            minlength: 1,
+            trim: true
+        }
+
+    });
+    let Libro = model('libros', libroSchema);
+    bookArray.forEach(async book => {
+        let libro = new Libro({
+            title: book.title,
+            author: book.author,
+            img: book.img
+        });
+        let p = await libro.save().then(resultado => {
+            console.log("Libro añadido: ", resultado);
+        }).catch(error => {
+            console.log("ERROR añadiendo libro: ", error)
         })
     })
-});
+}
+
